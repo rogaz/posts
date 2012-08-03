a90535ddb42dc33f7b357f7fdfc0c0b2f0db0088
Raul Ogaz <raul.26_90@hotmail.com>
2012-08-03 12:24:26 -0600

    Se realizaron cambios en estilos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,7 +66,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: 'Usuario Editado Correctamente' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
