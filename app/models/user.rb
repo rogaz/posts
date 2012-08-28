5c85c36c88750e71352babfb2a31b55e1afd0799
Raul Ogaz <raul.26_90@hotmail.com>
2012-08-28 09:21:47 -0600

    Contrasena con mas caracteres

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   validates :name, :presence => true, :length => {:within => 2..40}
   validates :email, :presence => true, :length => {:within => 5..30},
     :format => email_regex, :uniqueness => {:case_sensitive => false}
-  validates :password, :presence => true, :length => {:within => 2..10}, :confirmation => true
+  validates :password, :presence => true, :length => {:within => 2..30}, :confirmation => true
   validates :password_confirmation, :presence => true
   default_scope :order => "users.name ASC"
 
